e2a6250d3423b0b103badf14c8c5eefb1a6a8024
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-09-25 18:14:42 -0400

    polished classes and objects for future renders

--- js/app.js
+++ js/app.js
@@ -1,28 +1,32 @@
 //------------------------------CACHED Elements
 
-// bow elements
-const bowHeaderEl = document.querySelector("#bow > h2");
-const bowImgEl = document.querySelector("#bow > img");
-const bowDescription = document.querySelector("#bow > p");
+// CARD SLOT 1
+const card1HeaderEl = document.querySelector("#card1 > h2");
+const card1ImgEl = document.querySelector("#card1 > img");
+const card1Description = document.querySelector("#card1 > p");
 
-// Club elements
-const clubHeaderEl = document.querySelector("#club > h2");
-const clubImgEl = document.querySelector("#club > img");
-const clubDescription = document.querySelector("#club > p");
+// CARD SLOT 2
+const card2HeaderEl = document.querySelector("#card2 > h2");
+const card2ImgEl = document.querySelector("#card2 > img");
+const card2Description = document.querySelector("#card2 > p");
 
-// Axe elements
-const axeHeaderEl = document.querySelector("#axe > h2");
-const axeImgEl = document.querySelector("#axe > img");
-const axeDescription = document.querySelector("#axe > p");
+// CARD SLOT 3
+const card3HeaderEl = document.querySelector("#card3 > h2");
+const card3ImgEl = document.querySelector("#card3 > img");
+const card3Description = document.querySelector("#card3 > p");
 
-// Heal elements
-const healHeaderEl = document.querySelector("#heal > h2");
-const healImgEl = document.querySelector("#heal > img");
-const healDescription = document.querySelector("#heal > p");
+// CARD SLOT 4
+const card4HeaderEl = document.querySelector("#card4 > h2");
+const card4ImgEl = document.querySelector("#card4 > img");
+const card4Description = document.querySelector("#card4 > p");
+
+// Avatar IMGS
+const enemyEl = document.querySelector("#enemyAvatar");
+const playerEl = document.querySelector("#playerAvatar");
 
 
 //--------------------------------CLASSES
-class spell {
+class Card {
     constructor(name, img, cost, dmg, healing){
         this.name = name;
         this.img = img;
@@ -32,62 +36,76 @@ class spell {
     }
 }
 
-class player {
-    constructor(name,health,coins,spell1,spell2,spell3,spell4){
+class Player {
+    constructor(name,health,coins,card1,card2,card3,card4,img){
         this.name = name;
         this.health = health;
         this.coins = coins;
 
         // unnique spells
-        this.spell1 = spell1;
+        this.card1 = card1;
+        this.card2 = card2;
+        this.card3 = card3;
+        this.card4 = card4;
+
+        this.img = img;
     }
 }
 
-class monster {
-    constructor(name,health){
+class Monster {
+    constructor(name,health,img){
         this.name = name;
         this.health = health;
+        this.img = img;
     }
 }
 
 //----------------OBJECTS 
 
 //-----------Player spells - VIKING
-let bowSpell = new spell("Long Shot", "/imgs/bow.svg", 1, 80)
-let clubSpell = new spell("Skull Cracker", "/imgs/club.svg", 2, 130)
-let axeSpell = new spell("War Axe", "/imgs/axe.svg", 3, 200)
-let healSpell = new spell("Potion", "/imgs/heal.svg", 4, null, 250)
+let bowWeapon = new Card("Long Shot", "/imgs/bow.svg", 1, 80)
+let clubWeapon = new Card("Skull Cracker", "/imgs/club.svg", 2, 130)
+let axeWeapon = new Card("War Axe", "/imgs/axe.svg", 3, 200)
+let healPotion = new Card("Potion", "/imgs/heal.svg", 4, null, 250)
 
 // Players
-let viking = new player("Viking", 500, 1, bowSpell);
+let viking = new Player("Viking", 500, 1, bowWeapon,clubWeapon,axeWeapon,healPotion, "/imgs/viking.svg");
 console.log(viking);
 
+// Monsters 
+let saberTooth = new Monster("SaberTooth", 800, "/imgs/saberTooth.svg")
 
-function renderCards(){
+
+function renderViking(){
     
     // Bow rendered
-    bowHeaderEl.textContent = bowSpell.name;
-    bowImgEl.src = bowSpell.img;
-    bowDescription.textContent = `Damage: ${bowSpell.dmg}`;
+    card1HeaderEl.textContent = viking.card1.name;
+    card1ImgEl.src = viking.card1.img;
+    card1Description.textContent = `Damage: ${viking.card1.dmg}`;
 
-    // Club rendered
-    clubHeaderEl.textContent = clubSpell.name;
-    clubImgEl.src = clubSpell.img;
-    clubDescription.textContent = `Damage: ${clubSpell.dmg}`;
+    // Club card rendered
+    card2HeaderEl.textContent = viking.card2.name;
+    card2ImgEl.src = viking.card2.img;
+    card2Description.textContent = `Damage: ${viking.card2.dmg}`;
 
-    // Axe rendered
-    axeHeaderEl.textContent = axeSpell.name;
-    axeImgEl.src = axeSpell.img;
-    axeDescription.textContent = `Damage: ${axeSpell.dmg}`;
+    // Axe card rendered
+    card3HeaderEl.textContent = viking.card3.name;
+    card3ImgEl.src = viking.card3.img;
+    card3Description.textContent = `Damage: ${viking.card3.dmg}`;
 
-    // Heal rendered
-    healHeaderEl.textContent = healSpell.name;
-    healImgEl.src = healSpell.img;
-    healDescription.textContent = `Heal: ${healSpell.healing}`;
+    // Heal card rendered
+    card4HeaderEl.textContent = viking.card4name;
+    card4ImgEl.src = viking.card4.img;
+    card4Description.textContent = `Heal: ${viking.card4.healing}`;
+
+    // player avatars rendered 
+    playerEl.src = viking.img;
+
+    enemyEl.src = saberTooth.img
 
 }
 
-renderCards();
+renderViking();
 
 
 
@@ -108,3 +126,5 @@ renderCards();
 // 3. Create a class for different kind of players and enemies
     // make the enemy render the card it chooses randomly
     // 3.1 enemies are special and dont use coins just attacks
+
+    
